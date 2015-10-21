e7b34d696d888dd843f15c1b37c08d8fef5e88ca
huangbaoquan <baoquanhuang@gmail.com>
2015-10-21 22:02:54 +0800

    countZero

---
+++ countZero/moveZero.cpp
@@ -0,0 +1,53 @@
+//author : bqhuang
+//date   : 2015/10/21
+
+/********************************************************************************** 
+Given an array nums, write a function to move all 0's to the end of it while 
+maintaining the relative order of the non-zero elements.
+
+For example, given nums = [0, 1, 0, 3, 12], after calling your function, 
+nums should be [1, 3, 12, 0, 0].
+
+Note:
+1. You must do this in-place without making a copy of the array.
+2. Minimize the total number of operations.
+**********************************************************************************/ 
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+void moveZero(vector<int>& array)
+{
+	int len = array.size(); 
+	int position = 0;
+	for(int i=0;i<len;i++)
+	{
+		if(array[i] == 0)
+		{
+			while(position < len)
+			{
+				if(position < i)
+					position = i;
+				if(array[position] != 0)
+				{
+					swap(array[i],array[position++]);
+					break;
+				}
+				position++;
+			}
+		}
+	}
+}
+
+int main()
+{
+	int num[] = {0,1,0,3,12};
+	vector<int> array(num,num+5);
+	moveZero(array);
+	for(int i = 0;i<array.size();i++)
+	{
+		printf("%d ",array[i]);
+	}
+}
