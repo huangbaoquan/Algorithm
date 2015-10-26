092d883fc1f11b11304b059a9d980e7f739630d9
huangbaoquan <baoquanhuang@gmail.com>
2015-10-27 01:05:51 +0800

    add method to solve prime

--- prime/printPrime.cpp
+++ prime/printPrime.cpp
@@ -10,24 +10,57 @@
 #include <math.h>
 using namespace std;
 
+// 普通的解法,时间复杂度nlogn
 void printPrime()
 {
 	int i;
 	int j = 2;
-	for(i=2;i<=100;i++)
+	for(i=2;i<=200;i++)
 	{
-		for(j=2;j<i;j++)
+		for(j=2;j<sqrt(i);j++)
 		{
 			if(i%j == 0)
 			{
 				break;
 			}
 		}
-		if(j == i)
-			printf("%d ",i);
+		if(j > sqrt(i))
+			printf("%-4d",i);
 	}
 }
+// 优雅方法,此方法计算量少
+void siftPrime()
+{
+	bool num[200]; 
+	for(int i =0;i<200;i++)
+	{
+		num[i] = true;
+	}
+	// 0 , 1 is not prime
+	num[0] = false;
+	num[1] = false;
+	for(int i=2;i<200;i++)
+	{
+		if(num[i])
+		{
+			for(int j=i+i;j<200;j+=i)
+			{
+				num[j] = false;
+			}
+		}
+	}
+	for(int i=0;i<200;i++)
+	{
+		if(num[i])
+		{
+			printf("%-4d",i);
+		}
+	}
+}
+
 int main()
 {
 	printPrime();
+	printf("\n");
+	siftPrime();
 }
